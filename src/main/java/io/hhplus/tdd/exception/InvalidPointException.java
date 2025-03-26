fff516f4c86dc221489e69cd00771a77eadd896a
JiwooJng <jjjw1225@gmail.com>
2025-03-26 22:05:44 +0900

    * Exception 패키지 생성  * InvalidPointException   * 충전 혹은 사용 금액이 0원 이하인 경우 발생하는 exception  * InsufficientPointException   * 사용 금액이 잔고보다 많은 경우 발생하는 exception  * PointLimitExceedException   * 충전 한도가 넘을 경우 발생하는 exception
    * PointService
     * 충전 최대 한도 1,000,000 설정
      * 충전 최대 한도 넘는 경우 PointLimitExceedException 발생
     * 포인트 충전
      * 충전 금액 <= 0일 때 InvalidPointException 발생
      * 충전 금액 + 잔고 > 최대 한도일 때 PointLimitExceedException 발생
      * 정상 : 잔액 + 충전 금액 후 다시 유저 포인트 테이블과 히스토리 테이블에 업데이트
     * 포인트 사용
      * 사용 금액 <= 0일 때 InvalidPointException 발생
      * 사용 금액 > 잔고 일 때 InsufficientPointException 발생
      * 정상 : 잔액 - 사용 금액 후 유저 포인트 테이블과 히스토리 테이블에 업데이트
    
    * PointController
     * PointService 선언 및 PointService 에서 구현한 메소드 사용.
    
    (cherry picked from commit edfa68296313258903e43a6ccb203baf424750e5)

---
+++ src/main/java/io/hhplus/tdd/exception/InvalidPointException.java
@@ -0,0 +1,7 @@
+package io.hhplus.tdd.exception;
+
+public class InvalidPointException extends RuntimeException {
+    public InvalidPointException(String message) {
+        super(message);
+    }
+}
